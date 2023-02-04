6d20c6f03255d81e6223fcebca7426aa546f5bba
nick lehman <nlehman@combitsystems.com>
2023-02-04 23:51:36 +0000

    Add drawer

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import './App.css';
 import { LoremIpsum } from 'react-lorem-ipsum';
 
 import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
 
 import Footer from './Footer';
 import Header from './Header';
 
-// <ul>{Object.keys(window).map((e, i) => <li key={i}>{e} - {typeof window[e]}</li>)}</ul>
 function App() {
+  const drawerWidth = 250;
   return (
     <>
       <CssBaseline />
       <header>
         <Header />
       </header>
+      <Drawer variant="permanent" open anchor="left"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <ul>{Object.keys(window).map((e, i) => <li key={i}>{e} - {typeof window[e]}</li>)}</ul>
+      </Drawer>
       <main>
         <section>
           <LoremIpsum p={14} />
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,14 @@
-import Menu from "@mui/icons-material/Menu";
-import AppBar from "@mui/material/AppBar";
-import IconButton from "@mui/material/IconButton";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import Menu from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function Header() {
   return (
-    <AppBar position="static" color="primary">
+    <AppBar position="fixed" color="primary"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <Menu />
